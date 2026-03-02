e58a37f466298ed9467680e9e06652adbb5f751c
tirthaghara <tirathaghara@gmail.com>
2026-03-02 17:46:25 +0530

    fixed frontend dependencies

--- backend/db.js
+++ backend/db.js
@@ -1,29 +1,29 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/fingerscan/demo')
-  .then(() => console.log('MongoDB connected locally'))
-  .catch(err => console.error('MongoDB connection error:', err));
+// mongoose.connect('https://fingerscan-4.onrender.com')
+//   .then(() => console.log('MongoDB connected locally'))
+//   .catch(err => console.error('MongoDB connection error:', err));
 
-module.exports = mongoose;
+// module.exports = mongoose;
 
-// const mongoose = require("mongoose");
+const mongoose = require("mongoose");
 
-// const connectDB = async () => {
-//   try {
-//     if (!process.env.MONGO_URI) {
-//       throw new Error("MONGO_URI is not defined");
-//     }
+const connectDB = async () => {
+  try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined");
+    }
 
-//     await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI);
 
-//     console.log("✅ MongoDB connected successfully");
-//   } catch (error) {
-//     console.error("❌ MongoDB connection error:", error.message);
-//     process.exit(1);
-//   }
-// };
+    console.log("✅ MongoDB connected successfully");
+  } catch (error) {
+    console.error("❌ MongoDB connection error:", error.message);
+    process.exit(1);
+  }
+};
 
-// module.exports = connectDB;
+module.exports = connectDB;
 
 
 // mongodb+srv://tirthaghara_db:<db_password>@cluster0.gdd6zjn.mongodb.net/?appName=Cluster0";
